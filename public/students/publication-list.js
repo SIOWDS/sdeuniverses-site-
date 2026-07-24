5ba1f9f33f02fdd8571aa0f83ace3cc5f4c2747f
Wang Desheng <wangdesheng1234567@gmail.com>
2026-07-24 08:11:16 +0800

    Version student publication assets

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -205,7 +205,7 @@
     return section;
   }
 
-  fetch("/students/publications.json")
+  fetch("/students/publications.json?v=20260724-3")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
