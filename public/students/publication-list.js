2c14e67896d481275564ac42d66890bbdb4615e8
SIOWDS <wangdesheng1234567@gmail.com>
2026-07-28 08:36:07 +0000

    Launch The Finest channel with Qin Li's five polished essays

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -205,7 +205,7 @@
     return section;
   }
 
-  fetch("/students/publications.json?v=20260728-2")
+  fetch("/students/publications.json?v=20260728-finest")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
