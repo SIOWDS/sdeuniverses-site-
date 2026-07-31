15cb7d4925531da532d57ff638334a34c321d95b
SIOWDS <wangdesheng1234567@gmail.com>
2026-07-31 07:00:19 +0000

    Publish four reinforced papers by Gao Yuhan on the ontology of music

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260731-zq10")
+    fetch("/students/publications.json?v=20260731-gyh2")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
