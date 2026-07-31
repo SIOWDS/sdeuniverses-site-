08b3785ed6194d0308f36d5df30e5135326a93f7
SIOWDS <wangdesheng1234567@gmail.com>
2026-07-31 06:50:20 +0000

    Publish three papers by Jin Hua on judgement and multi-model thinking, each with an added nearest-neighbour cut

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260731-ly")
+    fetch("/students/publications.json?v=20260731-jh")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
