cdff658968ddcc88dda9b1c2cc5f3ee7fd599303
SIOWDS <wangdesheng1234567@gmail.com>
2026-07-31 07:20:01 +0000

    Publish Hu Zhiying top ten with nearest-neighbour cuts, decidable predictions, and two citation corrections

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260731-hmrc")
+    fetch("/students/publications.json?v=20260731-hzy10b")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
