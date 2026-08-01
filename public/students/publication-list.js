a9bfc81f5efdb26d6c8388748216f0e285f77f13
SIOWDS <wangdesheng1234567@gmail.com>
2026-08-01 11:48:52 +0000

    Publish 7 Chen Xiaoyan papers: three on the genesis of selfhood in evaluative parenting, four on the genesis of labour pricing, each with an editorial division-of-labour map explaining why 7 of 22 were selected

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260801-gpzq")
+    fetch("/students/publications.json?v=20260801-cx62")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
