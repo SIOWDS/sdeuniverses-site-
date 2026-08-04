313a54eff4606243ee410284e91c699f85278f82
SIOWDS <wangdesheng1234567@gmail.com>
2026-08-04 08:23:42 +0000

    Publish four Jin Hua papers on scale-free networks with nearest-neighbour reinforcement and an editor-supplied bibliography

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260804-hq5")
+    fetch("/students/publications.json?v=20260804-jh4")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
