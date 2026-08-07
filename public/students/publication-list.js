c3552e0e839de958ee75fa1b865c228e0983b295
SIOWDS <wangdesheng1234567@gmail.com>
2026-08-07 22:26:40 +0000

    feat(finest): admit Hu Min's nine chronic-disease papers with final-cut sections

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260808-hmcd9")
+    fetch("/students/publications.json?v=20260808-hmcd9f")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
