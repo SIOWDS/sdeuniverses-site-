ba63da08b6633ac742668679c16066fa917c7fe1
SIOWDS <wangdesheng1234567@gmail.com>
2026-08-08 13:53:35 +0000

    Publish five Sprachgefuehl papers and new language-sense channel for student Hu Zhiying

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260808-lei3")
+    fetch("/students/publications.json?v=20260808-ls5")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
