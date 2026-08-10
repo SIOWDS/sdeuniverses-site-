02f916284e9e7e757a85b38fd5e71acdede74cc1
SIOWDS <wangdesheng1234567@gmail.com>
2026-08-10 22:43:59 +0000

    Publish Cai Yan four reinforced papers on mistake-notebook (cognitive payoff-capacity, pre-wound, structural yielding, cognitive compensation)

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260809-gp3")
+    fetch("/students/publications.json?v=20260810-cai")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
