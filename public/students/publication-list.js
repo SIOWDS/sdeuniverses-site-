9049e1360c652af7071d49d933e4d4ae74cc0ed9
SIOWDS <wangdesheng1234567@gmail.com>
2026-08-11 10:10:25 +0000

    Publish Hu Min's Mediation channel: nine papers (three certified blind, six with editor-added claim-advancement notes), channel page, works cards, roster/publications mounts, homepage feature

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260810-cai")
+    fetch("/students/publications.json?v=20260811-hmmed")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
