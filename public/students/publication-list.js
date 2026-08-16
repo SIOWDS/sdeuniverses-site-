ce4cb716dfe4c4b8ef2636ecaa10d8abe4d07036
SIOWDS <wangdesheng1234567@gmail.com>
2026-08-16 00:06:47 +0000

    Align the tables and page totals to the half-weight companion rule

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260816-comp166")
+    fetch("/students/publications.json?v=20260816-half")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
