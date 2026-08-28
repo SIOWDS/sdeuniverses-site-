3f9261b2ef512fe743af6a4bf9bdab17c067b864
SIOWDS <wangdesheng1234567@gmail.com>
2026-08-28 07:56:28 +0000

    Publish Hu Min's Corporate Health channel: nine papers with five editor addenda

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260828-fs")
+    fetch("/students/publications.json?v=20260828-ch")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
