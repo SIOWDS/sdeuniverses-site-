415eb57a282fbbd7863269b1a686d4ea9a4e0624
SIOWDS <wangdesheng1234567@gmail.com>
2026-08-28 07:40:55 +0000

    Publish Hu Min's Fengshui channel: nine papers on fengshui as a verdict device

--- public/students/publication-list.js
+++ public/students/publication-list.js
@@ -216,7 +216,7 @@
     return section;
   }
 
-    fetch("/students/publications.json?v=20260816-half")
+    fetch("/students/publications.json?v=20260828-fs")
     .then(function (response) {
       if (!response.ok) throw new Error("Publication data unavailable");
       return response.json();
